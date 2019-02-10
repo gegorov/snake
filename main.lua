cb4f839985b9b3fa247a4fd80aa505444b876ed2
gegorov <gegorov@yahoo.com>
2019-02-10 22:32:16 +0300

    initial commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+
+function love.load()
+  -- body
+  love.window.setTitle('Snake50');
+end
+
+function love.keypressed(key)
+  -- body
+  if key == 'escape' then
+    -- body
+    love.event.quit();
+  end
+end
+
+function love.update(dt)
+  -- body
+end
+
+function love.draw()
+  -- body
+end
